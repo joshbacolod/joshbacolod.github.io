027b1abd719581ff27aa3d15c64f7ea768b4e579
joshbacolod <josh.bacolod@gmail.com>
2024-06-17 06:22:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+function myPopup(certificateId) {
+    var imgSrc = ""
+
+    document.querySelector(".popup").style.display = "block";
+    switch(certificateId) {
+      case "comptia":
+        imgSrc = "certificates/comptia-certificate.jpg";
+        break;
+      case "azure":
+        imgSrc = "certificates/azure-fundamentals.jpg";
+        break;
+      case "ms365":
+      imgSrc = "certificates/ms365-fundamentals.jpg";
+        break;
+    }
+    document.querySelector(".popup img").src = imgSrc;
+  }
+
+function closePopup() {
+  document.querySelector(".popup").style.display = "none";
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        };
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el))
+
+let lastScrollY = 0
+const navbar = document.querySelector(".header__items")
+
+window.addEventListener("scroll", () => {
+    const currentScrollY = window.scrollY
+    if (currentScrollY > lastScrollY) {
+        console.log("scroll down")
+        navbar.classList.add("hide");
+    } else {
+        console.log("scroll up")
+        navbar.classList.remove("hide");
+    }
+    lastScrollY = window.scrollY  
+});
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === "Escape" || event.key === "Esc" || event.keyCode === 27) {
+    closePopup();
+  }
+})
