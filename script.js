8f300712f162ae421a18b1d99cb7bdc71cb7ab3f
joshbacolod <josh.bacolod@gmail.com>
2025-01-12 04:22:10 +0800

    Functionality and class names updates
    changed class names to describe better
    
    added next and prev function to projects

--- script.js
+++ script.js
@@ -51,3 +51,18 @@ function myPopup(certificateId) {
 function closePopup() {
 document.querySelector(".popup").style.display = "none";
 }
+
+let slides = document.querySelectorAll(".project");
+let index = 0;
+
+function next() {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}
+
+function prev() {
+  slides[index].classList.remove("active");
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add("active");
+}
